6cb554e8309736ec2c282c28a0838bc31457968b
duk <dulguundwhrbyr@gmail.com>
2023-05-21 15:31:53 +0800

    first commit

---
+++ media.js
@@ -0,0 +1,217 @@
+let sar=16;
+let jil, honog;
+
+jil = sar/12;
+jil = math.floor
+honog = (sar % 12) * 30;
+
+console.log("jil=" jil, "honog = " + honog);
+
+
+
+
+
+let a,b,c,d,e
+a=20;
+b=30;
+c=40;
+d=50;
+e=60;
+
+
+if (a<b && a<c && a<d && a<e) {
+console.log("hamgiin baga nas" + a)
+}
+else if( b<a && b<c && b<d && b<e) {
+    console.log("hamgiin baga nas" + b)
+}
+else if( c<a && c<b && c<d && c<e) {
+    console.log("hamgiin baga nas" + c)
+}
+else if( d<a && d<b && d<c && d<e) {
+    console.log("hamgiin baga nas" + d)
+}
+else if( e<a && e<b && e<c && e<d) {
+    console.log("hamgiin baga nas" + d)
+}
+
+
+let sar;
+let jil;
+
+sar= 40;
+
+jil = sar/12;
+jil = sar%12
+jil = Math.floor(jil)
+sar = sar/12
+sar = sar % 12
+sar= Math.floor(sar)
+
+console.log("jil = " +jil, "sar = " +sar)
+
+
+
+let tsag,honog;
+
+
+tsag = 72;
+
+honog= tsag/24;
+honog = tsag%24
+
+
+console.log( "honog = "+ honog)
+
+
+
+let a;
+a = 32
+
+if (a % 3 == 0 && a % 5 ==0) {
+    console.log("Fizzbuzz")
+    
+}
+else if(a % 5 ==0) {
+    console.log("Buzz")
+}
+else if(a%3 ==0) {
+    console.log("Fizz")
+}
+else {
+    console.log("Error")
+}
+
+
+
+let a;
+a=7;
+
+if(b==1 || ) {
+    console.log("zun")
+}
+else if(a >= 9 && a<=11){
+    console.log("namar")
+}
+// else if(a >=12 && a<=2){
+//     console.log("ovol")
+// }
+else if(a )
+ 
+b=18;
+switch (b) {
+    case 1:
+    case 2:
+    case 12:
+        console.log("ovol")
+       break;
+    
+    case 3:
+    case 4:
+    case 5:
+        console.log("havar")
+        break;
+
+
+    case 6:
+    case 7:
+    case 8:
+        console.log("zun")
+       break;
+
+    case 9:
+    case 10:
+    case 11:
+        console.log("namar")
+        break;
+
+    default:
+        console.log("buruu")
+        break;
+}
+
+
+
+
+Array;
+
+let a=[7, 5, -2, 8, -9];
+
+console.log( a[0])
+console.log( a[1])
+console.log( a[2])
+console.log( a[3])
+console.log( a[4])
+
+console.log(a[0]+a[1]+a[2]+a[3]+a[4])
+
+if(a[0] <a[1] && a[0]<a[2] && a[0]<a[3] && a[0]<a[4]){
+    console.log("hamgiin baga array" + a[0])
+}
+else if(a[1] <a[0] && a[1]<a[2] && a[1]<a[3] && a[1]<a[4]){
+    console.log("hamgiin baga array" + a[1] )
+}
+else if(a[2] <a[0] && a[2]<a[1] && a[2]<a[3] && a[2]<a[4]){
+    console.log("hamgiin baga array" + a[2] )
+}
+else if(a[3] <a[0] && a[3]<a[2] && a[3]<a[1] && a[3]<a[4]){
+    console.log("hamgiin baga array" + a[3] )
+}
+else if(a[4] <a[0] && a[4]<a[2] && a[4]<a[3] && a[4]<a[1]){
+    console.log("hamgiin baga array" + a[4] )
+}
+
+
+
+if(a[0] >a[1] && a[0]>a[2] && a[0]>a[3] && a[0]>a[4]){
+    console.log("hamgiin ih array" + a[0] )
+}
+else if(a[1] >a[0] && a[1]>a[2] && a[1]>a[3] && a[1]>a[4]){
+    console.log("hamgiin ih array" + a[1] )
+}
+else if(a[2] >a[0] && a[2]>a[1] && a[2]>a[3] && a[2]>a[4]){
+    console.log("hamgiin ih array" + a[2] )
+}
+else if(a[3] >a[0] && a[3]>a[1] && a[3]>a[2] && a[3]>a[4]){
+    console.log("hamgiin ih array" + a[3] )
+}
+else if(a[4] >a[0] && a[4]>a[1] && a[4]>a[3] && a[4]>a[2]){
+    console.log("hamgiin ih array" + a[4] )
+}
+
+
+if(a[0] % 2 == 0) {
+    console.log("tegsh" + a[0])
+}
+ if(a[1] % 2 == 0){
+    console.log("tegsh" + a[1])
+}
+ if(a[2] % 2 == 0){
+    console.log("tegsh" + a[2])
+}
+ if(a[3] % 2 == 0){
+    console.log("tegsh" + a[3])
+}
+ if(a[4] % 2 == 0){
+    console.log("tegsh" + a[4])
+}
+
+
+
+
+if(a[0] < 0) {
+    console.log("sondgoi" + a[0])
+}
+if(a[1] < 0) {
+    console.log("sondgoi" + a[1])
+}
+if(a[2] < 0) {
+    console.log("sondgoi" + a[2])
+}
+if(a[3] < 0) {
+    console.log("sondgoi" + a[3])
+}
+if(a[4] < 0) {
+    console.log("sondgoi" + a[4])
+}
+
